3a1d4514e965976424bac369248dcd741307e7b1
Hojun Bun <ponnponn.p@gmail.com>
2020-09-02 21:55:53 +0900

    change header styles and functionalities

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,49 +1,77 @@
-import React, { useContext, FunctionComponent } from "react";
-import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Button,
+  Drawer,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useContext, FunctionComponent, useState } from "react";
+import { useHistory } from "react-router-dom";
 import CurrentUserContext from "../../contexts/user/UserContext";
-import { Header, Navigation } from "./styled-header";
 import { deleteAccount, signOut } from "../../firebase/firebase";
+
+const useStyles = makeStyles(() => ({
+  navBar: {
+    backgroundColor: "#fff",
+  },
+  heading: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
 
 const HeaderComponent: FunctionComponent = () => {
   const currentUser = useContext(CurrentUserContext);
+  const [isAbleToShowDrawer, setIsAbleToShowDrawer] = useState(false);
+  const history = useHistory();
+  const classes = useStyles();
+
   const onClickSignOut = () => {
+    setIsAbleToShowDrawer(false);
     signOut();
   };
 
   const onClickDeleteAccount = () => {
+    setIsAbleToShowDrawer(false);
     deleteAccount();
   };
 
+  const onClickMenuIcon = () => {
+    setIsAbleToShowDrawer(true);
+  };
+
+  const onClickLink = () => {
+    history.push("/");
+  };
+
+  const closeDrawer = () => {
+    setIsAbleToShowDrawer(false);
+  };
+
   return (
-    <Header>
+    <>
       {currentUser && currentUser.id ? (
-        <>
-          <h1>
-            <Link to="/">Ponmodoro</Link>
-          </h1>
-          <Navigation>
-            <div
-              onClick={onClickSignOut}
-              role="button"
-              onKeyUp={onClickSignOut}
-              tabIndex={0}
-            >
-              Signout
-            </div>
-            <div
-              onClick={onClickDeleteAccount}
-              role="button"
-              onKeyUp={onClickDeleteAccount}
-              tabIndex={0}
-            >
-              Delete Account
-            </div>
-          </Navigation>
-        </>
+        <AppBar position="static" className={classes.navBar}>
+          <Toolbar>
+            <IconButton onClick={onClickMenuIcon}>
+              <MenuIcon />
+            </IconButton>
+            <Button onClick={onClickLink}>Ponmodoro</Button>
+            <Drawer open={isAbleToShowDrawer} onClose={closeDrawer}>
+              <Button onClick={onClickSignOut}>Signout</Button>
+              <Button onClick={onClickDeleteAccount}>Delete Account</Button>
+            </Drawer>
+          </Toolbar>
+        </AppBar>
       ) : (
-        <h1>Ponmodoro</h1>
+        <div className={classes.heading}>
+          <Typography variant="h1">PONMODORO</Typography>
+        </div>
       )}
-    </Header>
+    </>
   );
 };
 
