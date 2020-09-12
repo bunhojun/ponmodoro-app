66205a823489dc8aad41bdf5d8dde64c2ed53030
Hojun Bun <ponnponn.p@gmail.com>
2020-09-12 22:28:56 +0900

    deleted unnecessarycomponents

--- src/components/button/styled-button.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { StyleProps } from "../common-style/common-style";
-
-const Button = styled.button<StyleProps>`
-  width: ${(props) => props.width};
-  cursor: pointer;
-`;
-
-export default Button;
