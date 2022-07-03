5df96af46f0db3aa50af9af0c5976108251be4ea
bunhojun <ponnponn.p@gmail.com>
2022-07-03 13:16:25 +0900

    fix style

--- src/components/authed-page/authed-page.tsx
+++ src/components/authed-page/authed-page.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useContext } from "react";
 import CurrentUserContext from "../../contexts/user/UserContext";
+import { Inner } from "../common-style/common-style";
 
 type Props = {
   children: ReactNode;
@@ -7,8 +8,8 @@ type Props = {
 
 export const AuthedPage = ({ children }: Props): JSX.Element => {
   const currentUser = useContext(CurrentUserContext);
-  if (!currentUser?.id) {
-    return <></>;
+  if (currentUser?.id) {
+    return <Inner />;
   }
   return <>{children}</>;
 };
